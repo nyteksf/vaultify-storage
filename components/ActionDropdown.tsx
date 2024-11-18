ae3117b127d9740470402529a9d1e851c9186429
nyteksf <email.nytek@gmail.com>
2024-11-17 21:27:40 -0800

    Update dropdown action in file card component

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -1,8 +1,34 @@
 import React from "react";
 
-const ActionDropdown = ({ file }: { file: File }) => {
+import {
+  Dialog,
+} from "@/components/ui/dialog";
 
-  return <div>ActionDropdown</div>;
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+const ActionDropdown = ({ file }: { file: File }) => {
+  return (
+    <Dialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </Dialog>
+  );
 };
 
 export default ActionDropdown;
