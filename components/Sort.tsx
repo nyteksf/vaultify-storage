492b727811fc92a5130d2b607d7d3a5b28f50d41
nyteksf <email.nytek@gmail.com>
2024-11-25 01:37:33 -0800

    Remove unused, chart.tsx

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import { usePathname, useRouter } from "next/navigation";
 
 import {
--- components/StorageMeterCard.tsx
+++
@@ -1,7 +0,0 @@
-const StorageMeterCard = () => {
-  return (
-    <div className="w-1/2 h-20 mb-10 bg-brand rounded-xl">AvailableStorageCard</div>
-  );
-};
-
-export default StorageMeterCard;
