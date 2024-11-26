a3485b0822f2b2a262be2196d5ebe06e6a557b82
nyteksf <email.nytek@gmail.com>
2024-11-25 18:39:37 -0800

    Update mobile auth logo

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -32,13 +32,13 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 
       <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
         <div className="mb-16 lg:hidden">
-            <Image 
-                src="/assets/icons/logo-full-brand.svg"
-                alt="logo"
-                width={224}
-                height={82}
-                className="h-auto w-[200px] lg:w-[250px]"
-            />
+          <Image
+            src="/assets/icons/logo-full-2-brand"
+            alt="logo"
+            width={224}
+            height={82}
+            className="h-auto w-[200px] lg:w-[250px]"
+          />
         </div>
         {children}
       </section>
